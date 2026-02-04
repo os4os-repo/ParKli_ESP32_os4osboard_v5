5a50ec596eb7dd6ad16899451fe2e5c423c0da41
TobiasKanaske <84714243+TobiasKanaske@users.noreply.github.com>
2026-02-04 20:24:36 +0100

    Create Test_Read_Analog_Inputs_TDS.ino

---
+++ src_code/Test_Read_Analog_Inputs_TDS/Test_Read_Analog_Inputs_TDS.ino
@@ -0,0 +1,58 @@
+//#include <OneWire.h>
+//#include <DallasTemperature.h>
+
+//#define loraNSS 5
+//#define loraRST 26
+//#define loraDOI0 4
+//#define loraDOI1 17
+
+//#define SCL_PIN 22
+//#define SDA_PIN 21
+
+#define ledPin 13
+
+//#define sparePin35 35
+//#define sparePin39 36
+//#define sparePin39 39
+
+#define vBatPin 34
+
+#define routedSensorPin3 32       //humTempOptEnPin
+#define routedSensorPin2 33       //optSensorPin
+#define loraRoutedSensorEnPin 27  //loraTdsEnPin
+
+#define routedSensorS0 25
+#define routedSensorS1 14
+#define routedSensorS2 16
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(F("Starting"));
+
+  //selecting 010=2
+  pinMode(routedSensorS0, OUTPUT);
+  digitalWrite(routedSensorS0, LOW);
+  pinMode(routedSensorS1, OUTPUT);
+  digitalWrite(routedSensorS1, HIGH);
+  pinMode(routedSensorS2, OUTPUT);
+  digitalWrite(routedSensorS2, LOW);
+
+  //defining the 2 and 3 screw terminal as inputs
+  pinMode(routedSensorPin2, INPUT);
+  pinMode(routedSensorPin3, INPUT);
+
+  //activating the senosrs
+  pinMode(loraRoutedSensorEnPin, OUTPUT);
+  digitalWrite(loraRoutedSensorEnPin, HIGH);
+}
+
+void loop() {
+  Serial.println("analog value of terminal 2");  
+  Serial.println(analogRead(routedSensorPin2));
+  Serial.println("");
+  Serial.println("analog value of terminal 3");
+  Serial.println(analogRead(routedSensorPin3));
+  Serial.println("============================");
+  delay(500);
+}
